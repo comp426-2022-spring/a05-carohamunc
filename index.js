7e29b9ed793e50170248fc3fde93fb76fa76c5c8
carolynhammond <caroham@live.unc.edu>
2022-04-28 23:39:15 -0400

    deleted repetition

--- index.js
+++ index.js
@@ -97,24 +97,6 @@ app.get("/app/", (req, res, next) => {
     res.end(res.statusCode+ ' ' +res.statusMessage)
 });
 
-/*app.use((req, res, next) => {
-  let logdata = {
-      remoteaddr: req.ip,
-      remoteuser: req.user,
-      time: Date.now(),
-      method: req.method,
-      url: req.url,
-      protocol: req.protocol,
-      httpversion: req.httpVersion,
-      status: res.statusCode,
-      referrer: req.headers['referer'],
-      useragent: req.headers['user-agent']
-  };
-  const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referrer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
-  const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referrer, logdata.useragent)
-  //console.log(info)
-  next();
-})*/
 
 function coinFlip() {
     var x = Math.round(Math.random());
